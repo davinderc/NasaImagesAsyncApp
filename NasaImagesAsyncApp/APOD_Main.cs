1d0149ab75549cc5eb1c4cdb97bebeeb778e47a2
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-06-19 15:29:55 +0200

    Create initial template for form.

---
+++ NasaImagesAsyncApp/APOD_Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NasaImagesAsyncApp
+{
+    public partial class ApodMain : Form
+    {
+        public ApodMain()
+        {
+            InitializeComponent();
+        }
+
+        private void APOD_Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NasaImagesAsyncApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NasaImagesAsyncApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// API to use: GET https://api.nasa.gov/planetary/apod
+        /// query params: date (YYYY-MM-DD, default today), hd (bool, default false), api-key (string, default DEMO_KEY)
+        /// personal key (needs to be saved in a JSON or something to remove it from here:
+        /// 9D6EVFHS4mLEdvx6ZtN5i2XRv84kkfL3OwJYJLuQ
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ApodMain());
+        }
+    }
+}
