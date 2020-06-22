0387b320b17c5f26966b73ffea099ffbae2fdb12
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-06-22 15:57:55 +0200

    Add tests to check for image content on valid Apod dates and KeyNotFoundException on dates without Apods.

--- ApodTests/ApodDownloaderTests.cs
+++ ApodTests/ApodDownloaderTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
 using NasaImagesAsyncApp;
@@ -7,32 +9,65 @@ namespace ApodTests
 {
     public class ApodDownloaderTests
     {
-        private const string _dateFormat = "yyyy-MM-dd";
+        
+        private DateTime _todaysDate = DateTime.Today;
+        private ApodDownloader apodDownloader = new ApodDownloader();
 
         [Fact]
         public void ShouldReturnApodUrlForTodaysDate()
         {
             // Arrange
-            var date = DateTime.Today.ToString(_dateFormat);
+            var date = _todaysDate;
 
             // Act
-            var response = ApodDownloader.BuildUrl(date);
+            var response = apodDownloader.BuildUrl(date);
 
             // Assert
-            response.Should().Contain("http://");
+            response.Should().Contain($"https://api.nasa.gov/planetary/apod?api_key=9D6EVFHS4mLEdvx6ZtN5i2XRv84kkfL3OwJYJLuQ&date=");
         }
 
         [Fact]
-        public void ShouldReturnApodImageUrl()
+        public async Task ShouldReturnApodImageUrl()
         {
             // Arrange
-            var date = DateTime.Today.ToString(_dateFormat);
-            ApodDownloader.BuildUrl(date);
+            var date = DateTime.Today;
+            var response = apodDownloader.BuildUrl(date);
 
             // Act
+            var imageUrl = await apodDownloader.GetImageUrl(response);
 
             // Assert
+            imageUrl.Should().Contain("https://apod.nasa.gov/apod/image/");
 
+        }
+
+        [Fact]
+        public async Task ShouldFailOnDateWithoutAnApod()
+        {
+            // Arrange
+            var date = new DateTime(2020, 06, 10);
+            var response = apodDownloader.BuildUrl(date);
+
+            // Act
+            Func<Task> getImage = async () => await apodDownloader.GetImageUrl(response);
+
+            // Assert
+            getImage.Should().Throw<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public async Task ShouldReturnStreamOfTypeImage()
+        {
+            // Arrange
+            var date = new DateTime(2020, 06, 22);
+
+            // Act
+            var apodUrl = apodDownloader.BuildUrl(date);
+            var imageUrl = await apodDownloader.GetImageUrl(apodUrl);
+            var imageResponse = await apodDownloader.GetImage(imageUrl);
+
+            // Assert
+            imageResponse.Content.Headers.ContentType.Should().Be(System.Net.Http.Headers.MediaTypeHeaderValue.Parse("image/jpeg"));
         }
     }
 }
