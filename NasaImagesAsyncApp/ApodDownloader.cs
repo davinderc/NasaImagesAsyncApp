061762d49e103751b4351ba89531d80b3123b06f
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-06-22 15:33:56 +0200

    Adjust methods to be object not static methods. Add method to download image given date.

--- NasaImagesAsyncApp/ApodDownloader.cs
+++ NasaImagesAsyncApp/ApodDownloader.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Linq;
+using System.Drawing;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using Newtonsoft.Json;
@@ -13,9 +12,12 @@ namespace NasaImagesAsyncApp
     public class ApodDownloader
     {
         private const string ApiKey = "9D6EVFHS4mLEdvx6ZtN5i2XRv84kkfL3OwJYJLuQ";
-        private static HttpClient _httpClient;
-        private static UriBuilder _uriBuilder;
-        private static NameValueCollection _query;
+        private const string _dateFormat = "yyyy-MM-dd";
+        private HttpClient _httpClient;
+        private UriBuilder _uriBuilder;
+        private NameValueCollection _query;
+        private string _baseDirectory = $"C:/Users/dach/Dev/cSharpTraining/NasaImagesAsyncApp/NasaImagesAsyncApp/";
+
         public ApodDownloader()
         {
             _httpClient = new HttpClient();
@@ -23,26 +25,37 @@ namespace NasaImagesAsyncApp
             _query = HttpUtility.ParseQueryString(_uriBuilder.Query);
             _query["api_key"] = ApiKey;
         }
-        public static string BuildUrl(string date)
+        public string BuildUrl(DateTime date)
         {
-            _query["date"] = date;
+            _query["date"] = date.ToString(_dateFormat);
             _uriBuilder.Query = _query.ToString();
             var url = _uriBuilder.ToString();
             return url;
         }
 
-        public static async Task<HttpResponseMessage> GetImage(string imageUrl)
+        public async Task<HttpResponseMessage> GetImage(string imageUrl)
         {
             return await _httpClient.GetAsync(imageUrl);
         }
 
-        public static async Task<string> GetImageUrl(string apodUrl)
+        public async Task<string> GetImageUrl(string apodUrl)
         {
+            
             var response = await _httpClient.GetAsync(apodUrl);
             var apodMetadataString = await response.Content.ReadAsStringAsync();
             var apodMetadata = JsonConvert.DeserializeObject<Dictionary<string, string>>(apodMetadataString);
             var imageUrl = apodMetadata["hdurl"];
             return imageUrl;
+        }
+
+        public async Task<Image> DownloadImageForDate(DateTime date)
+        {
+            var apodUrl = BuildUrl(date);
+            var imageUrl = await GetImageUrl(apodUrl);
+            var image = await GetImage(imageUrl);
+            var imageStream = Image.FromStream(await image.Content.ReadAsStreamAsync());
+            imageStream.Save(_baseDirectory + $"{ date.ToString(_dateFormat)}.jpeg");
+            return imageStream;
         }
     }
 }
