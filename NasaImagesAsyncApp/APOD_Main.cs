0d7f3517a4076f729278ad68137d313d5db1a348
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-06-22 15:36:30 +0200

    Set GetImage button click to download currently selected date's image.

--- NasaImagesAsyncApp/APOD_Main.cs
+++ NasaImagesAsyncApp/APOD_Main.cs
@@ -21,5 +21,15 @@ namespace NasaImagesAsyncApp
         {
 
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            var apodDownloader = new ApodDownloader();
+            var selectedDate = dateTimePicker1.Value;
+            var image = await apodDownloader.DownloadImageForDate(selectedDate);
+            pictureBox1.Image = (Image)(new Bitmap(image, new Size(pictureBox1.Width,pictureBox1.Height)));
+            button1.Enabled = true;
+        }
     }
 }
