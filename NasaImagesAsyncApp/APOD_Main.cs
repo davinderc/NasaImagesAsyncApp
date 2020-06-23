b20545745cc3241e0a4a4f25ce18aef8927d6f23
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-06-23 11:05:41 +0200

    Check for locally saved images and load, otherwise download from Apod website.

--- NasaImagesAsyncApp/APOD_Main.cs
+++ NasaImagesAsyncApp/APOD_Main.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 
 namespace NasaImagesAsyncApp
 {
     public partial class ApodMain : Form
     {
-        private ApodDownloader _apodDownloader;
-        public ApodMain()
+        private readonly ApodDownloader _apodDownloader;
+        private const string ImageFileExtension = ".jpg";
+        private Image _image;
+        private readonly string _baseDirectory;
+    public ApodMain()
         {
             InitializeComponent();
             _apodDownloader = new ApodDownloader();
+            _baseDirectory = _apodDownloader.BaseDirectory;
+            if (!Directory.Exists(_baseDirectory))
+            {
+                Directory.CreateDirectory(_baseDirectory);
+            }
         }
 
         private void APOD_Main_Load(object sender, EventArgs e)
@@ -22,10 +31,21 @@ namespace NasaImagesAsyncApp
         {
             button1.Enabled = false;
             var selectedDate = dateTimePicker1.Value;
-            var image = await _apodDownloader.DownloadImageForDate(selectedDate);
-            if (image != null)
+            var fileName = _apodDownloader.FormatDate(selectedDate);
+            
+            if (File.Exists(_baseDirectory + fileName))
             {
-                pictureBox1.Image = (Image)(new Bitmap(image, new Size(pictureBox1.Width, pictureBox1.Height)));
+                _image = Image.FromFile(_baseDirectory + fileName + ImageFileExtension);
+            }
+            else
+            {
+                _image = await _apodDownloader.DownloadImageForDate(selectedDate);
+                
+            }
+            if (_image != null)
+            {
+                _image.Save(_baseDirectory + fileName + ImageFileExtension, _image.RawFormat);
+                pictureBox1.Image = new Bitmap(_image, new Size(pictureBox1.Width, pictureBox1.Height));
             }
             button1.Enabled = true;
         }
